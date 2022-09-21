c9403b37c691e07af468beb39369580add80c1f4
pcheung <pika206@gmail.com>
2022-09-20 20:43:51 -0500

    update server for database config

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 
+import './config/database.js'
+
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as turtlesRouter } from './routes/turtles.js'
