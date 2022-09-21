d35956e9a454aead734743493b2d235710f6946e
pcheung <pika206@gmail.com>
2022-09-20 20:46:50 -0500

    add listener for mongoose connection

--- config/database.js
+++ config/database.js
@@ -1,3 +1,8 @@
 import mongoose from 'mongoose'
 
 mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+db.on('connected', function(){
+  console.log(`connected to MongoDB ${db.name} at ${db.host} : ${db.port}`)
+})
